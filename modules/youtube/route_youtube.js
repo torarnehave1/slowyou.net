c822603f3cfecb8b7f325b8ab7651c291b2ef156
torarnehave <torarnehave@gmail.com>
2024-05-05 10:54:07 +0200

    refactor: Update Python process command in route_youtube.js to use python3

--- modules/youtube/route_youtube.js
+++ modules/youtube/route_youtube.js
@@ -83,8 +83,8 @@ router.get('/search', async (req, res) => {
 
       const videoInfo = response.data.items[0]; // Get the first item from the response
   
-      const pythonProcess = spawn('python3', [join(__dirname, '..', 'public', 'youtubepar.py'), videoId]);
-  
+      //const pythonProcess = spawn('python3', [join(__dirname, '..', 'public', 'youtubepar.py'), videoId]);
+      const pythonProcess = spawn('python3', [join(__dirname, '..', '..', 'public', 'youtubepar.py'), videoId]);
       let transcript = '';
   
       pythonProcess.stdout.on('data', (data) => {
