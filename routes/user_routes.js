ec6ec60c2caaf361ef373cb75bb77410171467a9
torarnehave <torarnehave@gmail.com>
2024-06-09 17:02:45 +0200

    refactor: Update forgot password email link to use HTTPS
    This commit updates the forgot password email link in the user_routes.js file to use HTTPS instead of HTTP. By using HTTPS, the link ensures a secure connection for users when resetting their password. This enhancement improves the security of the application and protects user data.
    
    Refactor the code to enhance the security of the forgot password feature.

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -264,7 +264,7 @@ router.get('/forgot', async (req, res) => {
             from: process.env.EMAIL_USERNAME,
             to: user.username,
             subject: 'Password Reset',
-            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\nhttp://localhost:5000/a/reset-password/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`,
+            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\nhttps://slowyou.net/a/reset-password/${token}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`,
         };
 
         transporter.sendMail(mailOptions, (err, response) => {
