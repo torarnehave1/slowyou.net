54ac07591553cbd4a517956c7858986bb29ff78e
torarnehave <torarnehave@gmail.com>
2024-06-22 21:58:46 +0200

    refactor: Add /rudder route to pages.js

--- modules/webpages/pages.js
+++ modules/webpages/pages.js
@@ -86,6 +86,20 @@ router.get('/faq', (req, res) => {
     
   });
   
+  
+  router.get('/rudder', isAuthenticated, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('username');
+        //res.send(`You are authenticated as ${user.username}`);
+        res.sendFile(path.join(filePath, 'modules/test/rudderstack.html'));
+  
+    } catch (ex) {
+      console.error(ex);
+        res.status(500).send('An error occurred while processing your request.');
+    }
+      
+    
+  });
 
   router.get('/anno', isAuthenticated, async (req, res) => {
     try {
