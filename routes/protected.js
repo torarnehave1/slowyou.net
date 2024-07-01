3615f584cafeba498aa72fe0e7ce8074307faed2
torarnehave <torarnehave@gmail.com>
2024-07-01 17:09:39 +0200

    refactor: Update CSS for quotes in styles.css

--- routes/protected.js
+++ routes/protected.js
@@ -32,7 +32,7 @@ function isAuthenticated(req, res, next) {
 router.get('/protected', isAuthenticated, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('username');
-        res.send(`You are authenticated as ${user.username}`);
+        res.json({ Username: user.username });
     } catch (ex) {
         console.error(ex);
         res.status(500).send('An error occurred while processing your request.');
